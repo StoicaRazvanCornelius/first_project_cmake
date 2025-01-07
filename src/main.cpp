c02e0b1b5a95e51e88bb2c55be5b4603ddbd609e
Razvan Stoica <83972556+StoicaRazvanCornelius@users.noreply.github.com>
2025-01-08 00:43:37 +0200

    added versioning

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
-
+#include "../build/config.h"
 using namespace std;
 
 int main(int argc, char* argv[]){
+    if (argc < 2) {
+    // report version
+    std::cout << argv[0] <<"Project name:" << PROJECT_NAME << " Version " << PROJECT_VERSION << std::endl;
+    std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    return 1;
+   }
     cout<<"hello world 3333 " << argc;
     return 0;
 }
